8c2c36cd7f96e39fbb58345824c8ab263c6ec9d2
Marshall Pierce <marshall@palominolabs.com>
2012-06-22 14:23:24 -0700

    local stackmob works

--- app/store/Meats.js
+++ app/store/Meats.js
@@ -1,13 +1,15 @@
 Ext.define('sencha-stackmob-2.store.Meats', {
     extend: 'Ext.data.Store',
 
+    requires: ['sencha-stackmob-2.data.proxy.StackMob'],
+
     config: {
         model: 'sencha-stackmob-2.model.Meat',
         autoLoad: true,
         sorters: 'name',
         proxy: {
-            type: 'rest',
-            url: 'meats.json'
+            type: 'stackmob',
+            url: 'meat'
         }
     }
 });
